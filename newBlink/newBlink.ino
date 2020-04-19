d41658fd7f8384e1400b29adfcae90a9ae6b2ff9
Zinnu Ryne <zinnuryne@gmail.com>
2020-04-18 21:40:20 -0400

    newBlink sketch

---
+++ newBlink/newBlink.ino
@@ -0,0 +1,52 @@
+/*
+  Multi-Blink
+ Turns on LEDs on for one second, then off for one second, repeatedly.
+ 
+ */
+
+int led_b = 13;
+int led_g = 12;
+int led_y =  8;
+int led_r =  7;
+int led_w =  4;
+
+int dtime  = 350;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led_b, OUTPUT);
+  pinMode(led_g, OUTPUT);
+  pinMode(led_y, OUTPUT);
+  pinMode(led_r, OUTPUT);
+  pinMode(led_w, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led_b, HIGH);
+  digitalWrite(led_g, LOW);
+  digitalWrite(led_y, LOW);
+  digitalWrite(led_r, LOW);
+  digitalWrite(led_w, HIGH);
+  delay(dtime);
+  digitalWrite(led_b, LOW);
+  digitalWrite(led_g, HIGH);
+  digitalWrite(led_y, LOW);
+  digitalWrite(led_r, HIGH);
+  digitalWrite(led_w, LOW);
+  delay(dtime);
+  digitalWrite(led_b, LOW);
+  digitalWrite(led_g, LOW);
+  digitalWrite(led_y, HIGH);
+  digitalWrite(led_r, LOW);
+  digitalWrite(led_w, LOW);
+  delay(dtime);
+  digitalWrite(led_b, LOW);
+  digitalWrite(led_g, HIGH);
+  digitalWrite(led_y, LOW);
+  digitalWrite(led_r, HIGH);
+  digitalWrite(led_w, LOW);
+  delay(dtime);
+}
+
