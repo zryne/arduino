59046926bcc9a37ccc11e39edef56c300886a5ae
Zinnu Ryne <zinnuryne@gmail.com>
2020-04-22 22:08:29 -0400

    stepper motor code correction

--- newBlink/newBlink.ino
+++ newBlink/newBlink.ino
@@ -4,21 +4,20 @@
  
  */
 
-const int led_b =  13;
-const int led_g =  12;
-const int led_y =  11;
-const int led_r =  10;
-const int led_w =   9;
-const int dtime = 250;
-
-const int step_pin1  =    2;
-const int step_pin2  =    3;
-const int step_pin3  =    4;
-const int step_pin4  =    5;
-const int step_num   =    0;
+const int led_b      =   13;
+const int led_g      =   12;
+const int led_y      =   11;
+const int led_r      =   10;
+const int led_w      =    9;
+const int led_delay  =  250;
+const int step_1     =    2;
+const int step_2     =    3;
+const int step_3     =    4;
+const int step_4     =    5;
 const int step_delay =    2;
 const int step_revs  = 1000;
 
+int step_num = 0;
 
 void setup() {                
   pinMode(led_b, OUTPUT);
@@ -27,109 +26,108 @@ void setup() {
   pinMode(led_r, OUTPUT);
   pinMode(led_w, OUTPUT);
 
-  pinMode(step_pin1, OUTPUT);
-  pinMode(step_pin2, OUTPUT);
-  pinMode(step_pin3, OUTPUT);
-  pinMode(step_pin4, OUTPUT);
+  pinMode(step_1, OUTPUT);
+  pinMode(step_2, OUTPUT);
+  pinMode(step_3, OUTPUT);
+  pinMode(step_4, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led_b, HIGH);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, HIGH);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, HIGH);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, HIGH);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, HIGH);
   digitalWrite(led_w, HIGH);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_r, HIGH);
   digitalWrite(led_w, HIGH);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, HIGH);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_r, HIGH);
   digitalWrite(led_w, HIGH);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, HIGH);
   digitalWrite(led_g, HIGH);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_r, HIGH);
   digitalWrite(led_w, HIGH);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, HIGH);
   digitalWrite(led_g, HIGH);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_r, HIGH);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, HIGH);
   digitalWrite(led_g, HIGH);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, HIGH);
   digitalWrite(led_g, HIGH);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, HIGH);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
   digitalWrite(led_b, LOW);
   digitalWrite(led_g, LOW);
   digitalWrite(led_y, LOW);
   digitalWrite(led_r, LOW);
   digitalWrite(led_w, LOW);
-  delay(dtime);
+  delay(led_delay);
 
   OneStep(false);
   delay(step_delay);
   /*
   for (int a = 0; a < step_revs; a++) {
     OneStep(false);
-    delay(dtime);
+    delay(step_delay);
   }
   for (int a = 0; a < step_revs; a++) {
     OneStep(true);
-    delay(dtime);
+    delay(step_delay);
   }
   */
 }
@@ -139,55 +137,55 @@ void OneStep(bool dir) {
   if (dir) {
     switch (step_num) {
       case 0:
-        digitalWrite(step_pin1, HIGH);
-        digitalWrite(step_pin2, LOW);
-        digitalWrite(step_pin3, LOW);
-        digitalWrite(step_pin4, LOW);
+        digitalWrite(step_1, HIGH);
+        digitalWrite(step_2, LOW);
+        digitalWrite(step_3, LOW);
+        digitalWrite(step_4, LOW);
         break;
       case 1:
-        digitalWrite(step_pin1, LOW);
-        digitalWrite(step_pin2, HIGH);
-        digitalWrite(step_pin3, LOW);
-        digitalWrite(step_pin4, LOW);
+        digitalWrite(step_1, LOW);
+        digitalWrite(step_2, HIGH);
+        digitalWrite(step_3, LOW);
+        digitalWrite(step_4, LOW);
         break;
       case 2:
-        digitalWrite(step_pin1, LOW);
-        digitalWrite(step_pin2, LOW);
-        digitalWrite(step_pin3, HIGH);
-        digitalWrite(step_pin4, LOW);
+        digitalWrite(step_1, LOW);
+        digitalWrite(step_2, LOW);
+        digitalWrite(step_3, HIGH);
+        digitalWrite(step_4, LOW);
         break;
       case 3:
-        digitalWrite(step_pin1, LOW);
-        digitalWrite(step_pin2, LOW);
-        digitalWrite(step_pin3, LOW);
-        digitalWrite(step_pin4, HIGH);
+        digitalWrite(step_1, LOW);
+        digitalWrite(step_2, LOW);
+        digitalWrite(step_3, LOW);
+        digitalWrite(step_4, HIGH);
         break;
     }
   } else {
     switch (step_num) {
       case 0:
-        digitalWrite(step_pin1, LOW);
-        digitalWrite(step_pin2, LOW);
-        digitalWrite(step_pin3, LOW);
-        digitalWrite(step_pin4, HIGH);
+        digitalWrite(step_1, LOW);
+        digitalWrite(step_2, LOW);
+        digitalWrite(step_3, LOW);
+        digitalWrite(step_4, HIGH);
         break;
       case 1:
-        digitalWrite(step_pin1, LOW);
-        digitalWrite(step_pin2, LOW);
-        digitalWrite(step_pin3, HIGH);
-        digitalWrite(step_pin4, LOW);
+        digitalWrite(step_1, LOW);
+        digitalWrite(step_2, LOW);
+        digitalWrite(step_3, HIGH);
+        digitalWrite(step_4, LOW);
         break;
       case 2:
-        digitalWrite(step_pin1, LOW);
-        digitalWrite(step_pin2, HIGH);
-        digitalWrite(step_pin3, LOW);
-        digitalWrite(step_pin4, LOW);
+        digitalWrite(step_1, LOW);
+        digitalWrite(step_2, HIGH);
+        digitalWrite(step_3, LOW);
+        digitalWrite(step_4, LOW);
         break;
       case 3:
-        digitalWrite(step_pin1, HIGH);
-        digitalWrite(step_pin2, LOW);
-        digitalWrite(step_pin3, LOW);
-        digitalWrite(step_pin4, LOW);
+        digitalWrite(step_1, HIGH);
+        digitalWrite(step_2, LOW);
+        digitalWrite(step_3, LOW);
+        digitalWrite(step_4, LOW);
     }
   }
   
